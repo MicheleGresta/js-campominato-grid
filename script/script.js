11af3fe34773e2860486e821e9e2fc2678194a00
MicheleGresta <malachia_1997@hotmail.it>
2023-07-12 10:44:35 +0200

    comments

--- script/script.js
+++ script/script.js
@@ -26,18 +26,22 @@ function bottoneGioca() {
 
 
 function creaCaselle () {
+    // creo un ciclo per creare casella per casella, per un totale di numeroCaselle preso sopra
     for (let i = 0; i < numeroCaselle; i++) {
+        // creo elemento div o box o casella come ti pare
         const square = document.createElement("div");
+        // decido quante caselle devono stare in una riga, in questo caso faccio una radice del numeroCaselle
         const squaresPerRow = Math.sqrt(numeroCaselle); 
 
         square.classList.add("squares");
         square.textContent = (i+1).toString();
+        // assegno la grandezza delle caselle dividendo il loro 100% per il  risultato della radice sopra
         square.style.flexBasis = `calc(100% / ${squaresPerRow})`;
 
         square.addEventListener("click", function () {
-            square.classList.toggle("toggleColor");
-            
+            square.classList.toggle("toggleColor");            
         })
+        // attacco tutto al contenitore padre
         contenitoreGioco.append(square);
     }
 }
